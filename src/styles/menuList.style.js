186a049db01ec13025f1f07b6b976b93b956bc7a
mxncdee <72338626+mxncdee@users.noreply.github.com>
2021-01-12 20:37:52 +0100

    fine.

---
+++ src/styles/menuList.style.js
@@ -0,0 +1,32 @@
+export default {
+  container: {
+    paddingTop: 20,
+    paddingBottom: 40,
+    flex: 1,
+    backgroundColor:'#007dbc'
+  },
+  navItemStyle: {
+    padding: 10,
+    fontWeight: 'bold',
+    fontSize: 20,
+    color:'black'
+  },
+  sectionHeadingStyle: {
+    paddingVertical: 10,
+    paddingHorizontal: 5
+  },
+  // top menu
+  sectionHeadingTextStyle:{
+    fontWeight:'bold',
+    fontSize:25,
+    marginLeft:'auto',
+    marginRight:'auto',
+    marginBottom: 20,
+    color:'#fff',
+  },
+   // cerca text
+  buttonTextStyle:{
+    fontSize:18,
+    fontWeight:'bold'
+  }
+};
