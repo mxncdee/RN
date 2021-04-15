639fca0e9ff51fe51f57fc638eabf1f4c1b7bbc4
mxncdee <72338626+mxncdee@users.noreply.github.com>
2021-04-15 11:04:38 +0200

    .

--- config.js
+++
@@ -1,5 +0,0 @@
-'use strict';
-import {API_KEY,API_BASE_URL} from 'react-native-dotenv';
-
-exports.API_KEY = API_KEY;
-exports.API_BASE_URL = API_BASE_URL;
--- readme.md
+++
@@ -1,3 +0,0 @@
-# React Native - App News
-
-E' possibile  navigare in diverse categorie e cercare notizie.
